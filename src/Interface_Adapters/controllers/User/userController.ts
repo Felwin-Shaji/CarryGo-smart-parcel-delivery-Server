0e6b8fb770e3930ad5093262588d9d8d4201dc6d
Felwin Shaji <felwinshaji@gmail.com>
2025-12-20 23:19:53 +0530

    feat(user): add reset password controller and route

--- src/Interface_Adapters/controllers/User/userController.ts
+++ src/Interface_Adapters/controllers/User/userController.ts
@@ -4,15 +4,17 @@ import { ApiResponse } from "../../presenters/ApiResponse";
 import { inject, injectable } from "tsyringe";
 import { IGetUserProfileUseCase } from "../../../Application/interfaces/useCase_Interfaces/user/GetUserProfile.useCase";
 import { USER_MESSAGES } from "../../../Infrastructure/constants/messages/userMessage";
-import { BaseEditUserProfileRequestDto } from "../../../Application/Dto/User/user.dto";
+import { BaseEditUserProfileRequestDto, UserResetPasswordRequestDTO } from "../../../Application/Dto/User/user.dto";
 import { IEditUserProfileUseCase } from "../../../Application/interfaces/useCase_Interfaces/user/EditUserProfile.usecase";
 import { STATUS } from "../../../Infrastructure/constants/statusCodes";
+import { IUserReserUserPassword } from "../../../Application/interfaces/useCase_Interfaces/user/ReserUserPassword.usecase";
 
 @injectable()
 export class UserController implements IUserController {
     constructor(
         @inject("IGetUserProfileUseCase") private _getUserProfileUseCase: IGetUserProfileUseCase,
         @inject("IEditUserProfileUseCase") private _editUserProfileUseCase: IEditUserProfileUseCase,
+        @inject("IUserReserUserPassword") private _userReserUserPassword: IUserReserUserPassword,
     ) { }
 
     getUserProfile = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
@@ -43,6 +45,24 @@ export class UserController implements IUserController {
                 )
             )
 
+        } catch (error) {
+            next(error)
+        }
+    };
+
+    resetUserPassword = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+        try {
+            const dto = req.body as UserResetPasswordRequestDTO;
+            const userId = req.user?.id!;
+            
+            await this._userReserUserPassword.execute(userId,dto);
+
+            return res.status(STATUS.OK).json(
+                ApiResponse.success(
+                    USER_MESSAGES.RESET_PASSWORD
+                )
+            )
+            
         } catch (error) {
             next(error)
         }

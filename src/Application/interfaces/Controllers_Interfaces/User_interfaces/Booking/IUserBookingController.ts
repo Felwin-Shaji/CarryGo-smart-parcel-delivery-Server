fc5b96befca8b6d8ac296c80cea7b545b7aa34b8
Felwin Shaji <felwinshaji@gmail.com>
2025-12-23 21:39:31 +0530

    feat(booking): add booking interfaces and service contracts

---
+++ src/Application/interfaces/Controllers_Interfaces/User_interfaces/Booking/IUserBookingController.ts
@@ -0,0 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+
+export interface IUserBookingController  {
+    validatePincode(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+}

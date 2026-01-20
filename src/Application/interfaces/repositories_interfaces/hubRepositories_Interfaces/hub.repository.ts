5e437c5edc40909bf7c90978b573bd0e443e2eb9
Felwin Shaji <felwinshaji@gmail.com>
2026-01-21 03:18:45 +0530

    refactor(hub): extend hub and worker repositories

--- src/Application/interfaces/repositories_interfaces/hubRepositories_Interfaces/hub.repository.ts
+++ src/Application/interfaces/repositories_interfaces/hubRepositories_Interfaces/hub.repository.ts
@@ -1,5 +1,5 @@
 import { Hub } from "../../../../Domain/Entities/Hub/Hub";
-import { GetHubsDTO } from "../../../Dto/Hub/hub.dto";
+import { GetHubsDTO, updateHubKycStatusDTO } from "../../../Dto/Hub/hub.dto";
 import { ServiceableHubWithAgencyDTO } from "../../../Dto/User/Booking.dto";
 import { IBaseRepository } from "../base.repository";
 export interface PaginatedHubData {
@@ -11,6 +11,8 @@ export interface PaginatedHubData {
 }
 
 export interface IHubRepository extends IBaseRepository<Hub> {
+    getHubById(hubId:string):Promise<Hub>;
+    updateKycSatus(hubId: string, dto: updateHubKycStatusDTO):Promise<void>;
     getPaginatedHubsByAgency(agencyId: string,dto: GetHubsDTO):Promise<PaginatedHubData>;
     findServiceableAgenciesWithHubs(fromPincode: string, toPincode: string):Promise<ServiceableHubWithAgencyDTO[]>;
 }

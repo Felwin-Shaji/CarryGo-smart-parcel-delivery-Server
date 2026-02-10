86d513613b0d2aa2e9f65a09157a6d4c03e0c07e
Felwin Shaji <felwinshaji@gmail.com>
2026-02-10 09:15:22 +0530

    feat(user): add user overview and kyc status update use cases

---
+++ src/Application/useCase/User/Traveler/UpdateUserKycStatus.usecase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "tsyringe";
+import { IUpdateUserKycStatusUseCase } from "../../../interfaces/useCase_Interfaces/user/IUpdateuSERKycStatusUseCase";
+import { IUserRepository } from "../../../interfaces/repositories_interfaces/userRepositories_Interfaces/user.repository";
+import { KYCStatus } from "../../../../Infrastructure/Types/types";
+import { updateUserKycStatusDTO } from "../../../Dto/User/user.dto";
+import { AppError } from "../../../../Domain/utils/customError";
+import { USER_MESSAGES } from "../../../../Infrastructure/constants/messages/userMessage";
+import { STATUS } from "../../../../Infrastructure/constants/statusCodes";
+
+@injectable()
+export class UpdateUserKycStatusUseCase implements IUpdateUserKycStatusUseCase {
+    constructor(
+        @inject("IUserRepository") private _userRepository: IUserRepository
+    ) { }
+    async execute(userId: string, dto: updateUserKycStatusDTO): Promise<KYCStatus> {
+        const user = await this._userRepository.findById({ _id: userId });
+        if (!user) throw new AppError(USER_MESSAGES.NOT_FOUND, STATUS.NOT_FOUND);
+
+        const updatedUser = await this._userRepository.findOneAndUpdate(
+            { _id: userId },
+            { kycStatus: dto.kycStatus, rejectReason: dto.rejectReason || null }
+        );
+        if (!updatedUser) throw new AppError(USER_MESSAGES.UPDATE_FAILED, STATUS.INTERNAL_SERVER_ERROR);
+
+        return updatedUser.kycStatus;
+    }
+}

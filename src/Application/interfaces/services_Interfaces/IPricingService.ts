cbaa55c2b6c850cc4da0e05d300f8d80323dd897
Felwin Shaji <felwinshaji@gmail.com>
2026-02-17 18:49:08 +0530

    refactor(pricing): remove legacy pricing service and calculation use case

--- src/Application/interfaces/services_Interfaces/IPricingService.ts
+++
@@ -1,9 +0,0 @@
-import { CalculatePriceRequestDTO } from "../../Dto/User/Booking.dto";
-import { CalculatePriceResponseDTO } from "../../Dto/User/Booking.dto";
-
-export interface IPricingService {
-  calculate(
-    userId: string,
-    dto: CalculatePriceRequestDTO
-  ): Promise<CalculatePriceResponseDTO>;
-}

ec6e1bb1df3b3825ddf2a5e6c91bff9fcae7adbf
Felwin Shaji <felwinshaji@gmail.com>
2026-02-17 18:51:02 +0530

    refactor(pricing): adjust related repositories and admin controller interfaces

--- src/Application/interfaces/repositories_interfaces/userRepositories_Interfaces/ITravelRequestRepository.ts
+++ src/Application/interfaces/repositories_interfaces/userRepositories_Interfaces/ITravelRequestRepository.ts
@@ -3,5 +3,5 @@ import { TravelRequest } from "../../../../Domain/Entities/User/TravelRequest";
 export interface ITravelRequestRepository {
   create(travelRequest: TravelRequest): Promise<void>;
   findByTravelerId(travelerId: string): Promise<TravelRequest[]>;
-  getTravelRequestById(travelerId: string, travelRequestId: string): Promise<TravelRequest>;
+  getTravelRequestById(travelRequestId: string): Promise<TravelRequest>;
 }
--- src/Interface_Adapters/Interface/Controllers_Interfaces/Admin_Interfaces/adminPricingPolicy.controller.ts
+++ src/Interface_Adapters/Interface/Controllers_Interfaces/Admin_Interfaces/adminPricingPolicy.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 export interface IAdminPricingPolicyController {
-    getAdminPricing(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
-    createAdminPricing(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+    getAdminAgencyPricing(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
+    createAdminAgencyPricing(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
 }
